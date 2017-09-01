dc80a0b44fabae0fd59177f1d9c91925638e833f
urlgo <urlgo@gmail.com>
2017-09-01 09:09:22 +0530

    Enhanced

--- src/com/raja/projects/studentdatabase/Main.java
+++ src/com/raja/projects/studentdatabase/Main.java
@@ -1,15 +1,18 @@
 package com.raja.projects.studentdatabase;
 
+import java.sql.SQLException;
+
 
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		String userName = "root";
 		String passWord = "";
 		String DBURL = "jdbc:mysql://localhost/studentdb";
 		StudentDatabaseHelper helper = new StudentDatabaseHelper(DBURL, userName, passWord);
 		Student s = new Student("Raja", "888928398", "raja@gmail.com", "coimbatore");
 		helper.pushData(s);
+		helper.viewData();
 	}
 
 }
--- src/com/raja/projects/studentdatabase/StudentDatabaseHelper.java
+++ src/com/raja/projects/studentdatabase/StudentDatabaseHelper.java
@@ -1,6 +1,7 @@
 package com.raja.projects.studentdatabase;
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.mysql.jdbc.Statement;
@@ -9,6 +10,7 @@ import com.mysql.jdbc.Connection;
 public class StudentDatabaseHelper {
 	Connection dbConnection;
 	Statement dbStatement;
+	ResultSet dbResults;
 	public StudentDatabaseHelper(String DBURL, String userName, String passWord){
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -36,15 +38,48 @@ public class StudentDatabaseHelper {
 		return pushQuery;
 		
 	}
-	public boolean pushData(Student a){
+	public boolean pushData(Student a) throws SQLException{
 		String queryDataPush = pushQueryBuilder(a);
+		if(!isAvailable(a)){
 		try {
+			
 			dbStatement.executeUpdate(queryDataPush);
 			System.out.println("Data Pushing Done :" +a.name);
 		} catch (SQLException e) {
 			System.out.println("Data Pushing Problem :" +a.name);
 			e.printStackTrace();
 		}
+		}
 		return true;
+	}
+	public boolean viewData(){
+		String queryDataGet = "select * from studentdetails";
+		try {
+			dbResults = dbStatement.executeQuery(queryDataGet);
+			System.out.println("We got the results");
+		} catch (SQLException e) {
+			System.out.println("Problem in getting dataset");
+		}
+		
+		System.out.println("Name\t\tPhoneNo\tEmail\tLocation");
+		
+		try {
+			while(dbResults.next()){
+				System.out.println(dbResults.getString(1)+"\t\t"+dbResults.getString(2)+"\t"+dbResults.getString(3)+"\t"+dbResults.getString(4));
+			}
+		} catch (SQLException e) {
+			System.out.println("Problem in reading dataset");
+		}
+		return true;
+		
+	}
+	public boolean isAvailable(Student aStudent) throws SQLException{
+		
+		String matchQuery = "select * from studentdetails where phonenumber like '"+ aStudent.phoneNo+"';";
+		dbResults = dbStatement.executeQuery(matchQuery);
+		if(dbResults.wasNull())
+			return true;
+		else
+			return false;
 	}
 }
